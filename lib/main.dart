3acb19e8643abaf9f8126fd29320e5526150cf73
Dongd <choise154@gmail.com>
2019-05-16 15:37:54 +0900

    finish step 2

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,72 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('StartUp Nmae Generaotr',
-            style: TextStyle(color: Colors.amber)
-        )
+          title: Text('StartUp Nmae Generaotr', style: TextStyle(color: Colors.amber)),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont
+                  )
+              );
+            }
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles
+        ).toList();
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestions')
+          ),
+          body: ListView(children: divided)
+        );
+      })
+    );
+  }
+
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
         title: Text(
             pair.asPascalCase,
             style: _biggerFont
-        )
+        ),
+
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if(alreadySaved) {
+              _saved.remove(pair);  
+            } else {
+              _saved.add(pair);
+            }
+          });
+        },
     );
   }
 
